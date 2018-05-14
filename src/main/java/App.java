195d94a1f7c0f02374b4f390ee62412fc6057e74
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-05-14 11:52:24 +0300

    creat an array list to accept multiple inputs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -9,22 +10,30 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (request, response) -> {
+    post("/heros", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+  
+      ArrayList<Hero> heros = request.session().attribute("heros");
+      if (heros == null) {
+        heros = new ArrayList<Hero>();
+        request.session().attribute("heros", heros);
+      }
+  
+      String description = request.queryParams("description");
+      Hero newHero = new Hero(description);
+      heros.add(newHero);
+  
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+
+     get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("heros", request.session().attribute("heros"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/heros", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-
-      String description = request.queryParams("description");
-      Hero newHero = new Hero(description);
-      request.session().attribute("Thor", newHero);
-
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
 
   }
 }
